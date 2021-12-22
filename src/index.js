06eb0702f4400ca148f8469bde2458b776ec635f
marko <pekica.marko@gmail.com>
2021-12-22 09:37:11 +0100

    added storage, few routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import cors from 'cors'
+import storage from './storage.js'
+
+console.log(storage)
+
+
 
 const app = express() // instanciranje aplikacije
+app.use(cors( ))    // bit ce koristen na svim rutama
 const port = 3000 // port na kojem će web server slušati
 
-app.get('/', (req, res) => res.send('Hellp!'))
+app.get('/', (req, res) => res.send('uh'))
+
+app.get('/activities', (req, res) => {
+    res.json(storage.activities)
+})
+
+app.get('/sessions', (req, res) => {
+    res.json(storage.sessions)
+})
+
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
