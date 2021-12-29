5b95c567f2f5bf6c1ee74ce238d4ed4f81ae9f23
marko <pekica.marko@gmail.com>
2021-12-29 14:33:39 +0100

    post

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ console.log(storage)
 
 
 const app = express() // instanciranje aplikacije
-app.use(cors( ))    // bit ce koristen na svim rutama
 const port = 3000 // port na kojem će web server slušati
+
+app.use( cors( ) )    // bit ce koristen na svim rutama
+app.use( express.json( ) )
 
 app.get('/', (req, res) => res.send('uh'))
 
@@ -17,13 +19,18 @@ app.get('/activities', (req, res) => {
     res.json(storage.activities)
 })
 app.post('/activities', (req, res) => {
-    res.statusCode = 201
-    res.setHeader('Location', '/activities/staJaToRadim')
-    res.send()
+    let activity = req.body
+    storage.activities.push(activity)
+    res.json("ok")
 })
 
 app.get('/sessions', (req, res) => {
     res.json(storage.sessions)
+})
+app.post('/sessions', (req, res) => {
+    let session = req.body
+    storage.sessions.push( session )
+    res.json('ok')
 })
 
 app.listen(port, () => console.log(`Slušam na portu ${port}!`))
