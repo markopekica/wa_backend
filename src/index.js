cf568ef6ce8361f0b610519121b4943d0231c1ec
marko <pekica.marko@gmail.com>
2022-02-05 16:16:02 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import connect from "./db.js";
 import auth from "./auth.js";
 
 const app = express(); // instanciranje aplikacije
-const port = 3000; // port na kojem će web server slušati
+const port = process.env.PORT; // port na kojem će web server slušati
 
 app.use(cors()); // bit ce koristen na svim rutama
 app.use(express.json());
